970887f3ad74cbf8f951017dd58dd506156a5371
aloisiot <loistc.carvalho@gmail.com>
2021-06-07 17:32:50 -0300

    Primeira estrutura condicional basica

---
+++ if.js
@@ -0,0 +1,8 @@
+let clima = 'quente';
+let dia = 'quarta';
+
+if (clima == 'quente' && dia == 'domingo') {
+    console.log('Vou tomar sorvete.');
+} else {
+    console.log('Ficar em casa estudando.');
+}
