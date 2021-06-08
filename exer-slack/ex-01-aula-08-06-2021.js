3ac1e496345f01e9ec0b1eb1200f80256770960c
aloisiot <loistc.carvalho@gmail.com>
2021-06-08 10:29:40 -0300

    Resolvendo dessafionas com estruturas IF ELSE e SWITCH propostas no Slack.

---
+++ exer-slack/ex-01-aula-08-06-2021.js
@@ -0,0 +1,11 @@
+function podeSubir(altura, acompanhante) {
+    if (altura > 1.40 && altura < 2) {
+        console.log('Acesso altorizado!');
+    } else if (altura <= 1.40 && acompanhante) {
+        console.log('Acesso altorizado somente com acompanhante!');
+    } else if (altura < 1.2) {
+        console.log('Acesso negado mesmo com acompanhante!')
+    }
+}
+
+podeSubir(1.70, true);
---
+++ exer-slack/ex-02-aula08-06-2021.js
@@ -0,0 +1,12 @@
+function estacoes(estacao) {
+    switch (estacao) {
+        case estacao == "Verão" : console.log("Que calor hein?!");
+        break
+        case estacao == "Inverno" : console.log("Frio dms!");
+        break
+        case estacao == "Outono" : console.log("É a estação em que as folhas caem.");
+        break
+        case estacao == "Primavera" : console.log("O momento em que as folhas crescem.");
+        default : console.log("Ops, estação inválida!")
+    }
+}
