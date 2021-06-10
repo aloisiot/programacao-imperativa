a05f379e969f1c76c0336ac95e5ff3de28cbba90
aloisiot <loistc.carvalho@gmail.com>
2021-06-10 14:04:00 -0300

    Arvore de natal com JS.

---
+++ extra-classe/arvore-de-natal.js
@@ -0,0 +1,11 @@
+let x = [10,2];
+
+x.forEach(row => {
+    new Array(row).fill('').forEach((v, i) => {
+        console.log([
+            ...new Array(9-i).fill('.'),
+            ...new Array(1+i*2).fill('^'),
+            ...new Array(9-1).fill('.')
+        ].join(''));
+    });
+});
