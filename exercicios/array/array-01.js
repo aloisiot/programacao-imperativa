325604cd1452be98fde01ae9e241e7ad3a607f66
aloisiot <loistc.carvalho@gmail.com>
2021-06-10 10:57:56 -0300

    Reajustando as descriçoes de acordo com o solicitado na ativide]

--- exercicios/array/array-01.js
+++ exercicios/array/array-01.js
@@ -1,24 +1,19 @@
 let cores = ['Azul', 'Branco', 'Verde'];
 
-// Inserindo novo elemento à última posição do array.
+console.log('O método push inserindo novo elemento à última posição do array.');
 cores.push('Lilas', 'Laranja');
 
-/* 
-O metodo pop() remove ultimo elemento do array.
-Alem de remover o elemento retorna o valor do mesmo, podendo assim ser armazenado em uma variável. 
-*/
+
+console.log('O metodo pop() remove ultimo elemento do array.\n Alem de remover o elemento retorna o valor do mesmo, podendo assim ser armazenado em uma variável.');
 let ultimaCor = cores.pop();
 console.log(ultimaCor);
 console.log(cores);
 
-/*
-O metodo shift() remove o elemento de indice 0 do array.
-Também retorna o elemento removido, podendo assim ser armazenado em uma variável.
-*/
+console.log('Também retorna o elemento removido, podendo assim ser armazenado em uma variável.\n');
 let primeitoElemento = cores.shift();
 console.log(primeitoElemento);
 console.log(cores);
 
-//O unshift() insere um ou mais elementos no inicio do array.
+console.log('O unshift() insere um ou mais elementos no inicio do array.');
 cores.unshift('Roxo');
 console.log(cores);
--- exercicios/array/array-02.js
+++ exercicios/array/array-02.js
@@ -1,16 +1,15 @@
 let pessoas = ['Ana', 'Lúcia', 'Márcio', 'Tereza', 'Ana', 'Ana'];
 
-// O método indexOf retorna o ìndice de um elemento em um array.
+console.log('O método indexOf retorna o ìndice de um elemento em um array.');
 console.log(pessoas.indexOf("Márcio"));
 
 
-// O métofo lastIndexOf retorna a últma ocorrência de determinado elemento em um array.
+console.log('O métofo lastIndexOf retorna a últma ocorrência de determinado elemento em um array.');
 console.log(pessoas.lastIndexOf('Ana'));
 
-// Recebe uma string como parametro.
-// Junta os elementos do array usando o parâmetro como separador dos elementos.
+console.log('O método join recebe uma string como parametro.\nJunta os elementos do array usando o parâmetro como separador dos elementos.')
 p = pessoas.join(',');
 console.log(p);
 
-// O método lengh retorna o comprimento de um array (total de elementos contidos).
+cosnole.log('O método lengh retorna o comprimento de um array (total de elementos contidos).');
 console.log(pessoas.length);
