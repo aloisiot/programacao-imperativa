13ab812af525c2507c2ea19db120253ab92440be
aloisiot <loistc.carvalho@gmail.com>
2021-06-15 18:24:13 -0300

    Resoluçao das atividades de Arrow function

---
+++ microdesafios-assincronos/arrow-functions/calculos-algebricos-basicos.js
@@ -0,0 +1,15 @@
+let soma = (a, b) => a + b;
+
+let sub = (a, b) => a - b;
+
+let mult = (a, b) => a * b;
+
+let div = (a, b) => a / b;
+
+let modulo = (a, b) => a % b;
+
+console.log( 8+' + ' + 4 + ' = ' + soma(8,4));
+console.log( 8+' - ' + 4 + ' = ' + sub(8,4));
+console.log( 8+' x ' + 4 + ' = ' + mult(8,4));
+console.log( 8+' / ' + 4 + ' = ' + div(8,4));
+console.log( 8+' % ' + 4 + ' = ' + modulo(8,4));
