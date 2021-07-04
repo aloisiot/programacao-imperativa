bd68665da7a7693a9141348cdf82995359ceb485
aloisiot <loistc.carvalho@gmail.com>
2021-07-04 10:37:00 -0300

    Resoluçao de atividades sobre metodos avançados de array

---
+++ microdesafios/array-avancado/filter.js
@@ -0,0 +1,5 @@
+let nomes = ['Maria', 'Ana', 'José', 'Maria', 'Davi'];
+
+let marias = nomes.filter(nome => nome == 'Maria');
+
+console.log(marias);
---
+++ microdesafios/array-avancado/map.js
@@ -0,0 +1,5 @@
+let pares = [2, 4, 6, 8];
+
+let impares = pares.map(e => e-1);
+
+console.log(impares);
