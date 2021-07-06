bccfc82643e94d845fd887f6ce5f78cd08c923d4
aloisiot <loistc.carvalho@gmail.com>
2021-07-06 19:37:09 -0300

    Resoluçao de exercicio untegrados 06/07/2021

---
+++ microdesafios/06-07-2021-contabilidade-agricola/metodos.js
@@ -0,0 +1,43 @@
+const produtos = require('./produtos');
+
+// Busca por nome:
+function buscador(produtos, nome){
+    return produtos.filter(element=>element.nome == nome)
+}
+
+// Calcular o lucro total de todos os produtos:
+function calculaTotal(produtos) {
+    return produtos.reduce((acum, elem)=> acum+elem.preco*elem.numVendas,0)
+}
+
+// Permitir pesquisar um produto pelo nome e calcular seu lucro total;
+function totalDoElementoX(produtos, nome){
+    return buscador(produtos,nome).reduce((acum,elem)=>acum+elem.preco*elem.numVendas,0)
+}
+
+
+// Permite receber um valor e indicar todos os produtos que venderam mais que esse valor;
+function vendasAcimaDeX(produtos, x){
+    return produtos.filter(produto=>produto.preco*produto.numVendas> x);
+}
+
+// Classifique todos os produtos pela quantidade de dinheiro que geraram.
+// Ordenar pelos mais vendidos:
+
+function maisVendidosAcima(produtos){
+    result = produtos.sort((produtoA, produtoB)=> {
+        return (produtoB.preco*produtoB.numVendas)-(produtoA.preco*produtoA.numVendas)
+    });
+
+    result.forEach(produto => produto.precoTotal = produto.preco*produto.numVendas);
+    return result
+}
+
+// Encontre um produto e atribua a ele um novo valor 
+// (reutilize o código que permite encontrar o produto pelo nome).
+function aleatoria(produt, nome){
+    result = produt.filter(element=>element.nome == nome)
+    const index = produt.indexOf(result[0]);
+    console.log(index);
+    produt[index].nome = 'Melancia';
+}
---
+++ microdesafios/06-07-2021-contabilidade-agricola/produtos.js
@@ -0,0 +1,15 @@
+// Temos que fazer um sistema para um Agricultor controlar suas vendas.
+// Ele tem um controle de vendas em um caderno,
+// onde os produtos estão organizados por nome, preço e quantidade vendida. 
+// Portanto, crie um array de objetos literais com essas informações.
+
+module.exports=[
+    {nome:'Alface', preco: 1.5, numVendas: 15},
+    {nome:'Beterraba', preco: 3.5, numVendas: 10},
+    {nome:'Cenoura', preco: 4, numVendas: 35},
+    {nome:'Rucula', preco: 2, numVendas: 8},
+    {nome:'Brocolis', preco: 4.5, numVendas: 48},
+    {nome:'Couve', preco: 2.8, numVendas: 18},
+    {nome:'Laranja', preco: 3.5, numVendas: 22},
+    {nome:'Limão', preco: 1.5, numVendas: 30},
+];
