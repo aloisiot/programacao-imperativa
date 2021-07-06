d23a9df3305d5ef68d7df418ba9b6254fe37de6a
aloisiot <loistc.carvalho@gmail.com>
2021-07-06 11:20:35 -0300

    add descriçao do metodo slice

---
+++ microdesafios/array-avancado/slice.js
@@ -0,0 +1,10 @@
+// Este método retorna uma compia do array em questão.
+// Para isso determinamos dois parâmetros, índice inicial e final (opcionais)
+
+const nums = [1,34,4353,23,55,5,63];
+
+const subNums = nums.slice(3,5);
+console.log(numsResum);
+
+// Obs: ao contrário do que se pode cpensar
+//      a interação retorna como ultimo elemento o elemento de indice final-1.s
