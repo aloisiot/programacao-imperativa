0ce992e5400d16314ec2a3e53858e90540bc021d
aloisiot <loistc.carvalho@gmail.com>
2021-07-06 12:18:02 -0300

    add descriçao do metodo array.includes()

---
+++ microdesafios/array-avancado/includes.js
@@ -0,0 +1,10 @@
+// Verifica a existência de determinado elemento no array
+// Retorna um valor boleano
+// Parâmetro:
+//      elemento: valor a ser procurado.
+//      inicio: índice ao qual se inicia a busca.
+
+let nomes = ['Maria', 'Ana', 'José', 'Davi'];
+
+console.log(nomes.includes('Ana')); // true
+console.log(nomes.includes('Marcos')); // false
