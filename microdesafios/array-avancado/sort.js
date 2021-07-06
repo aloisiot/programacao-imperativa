6c6c8087ea334bf49c7a7ea21bfeb9062b11ae8d
aloisiot <loistc.carvalho@gmail.com>
2021-07-06 11:48:36 -0300

    add descriçao do metodo de ordenaçao sort

---
+++ microdesafios/array-avancado/sort.js
@@ -0,0 +1,17 @@
+// O método sort nos ajuda a ordenar os elementos de um Array.
+// Recebe uma callback como parâmetro, que especifica o metodo de classificação. *opcional*
+// Se form omitido o array é ordenado com o valor da string(Unicode) e cada elemento é convertido em string.
+
+// Para ordenar valores numéricos a callback e obrigatória.
+const nums = [1,34,4353,23,55,5,63];
+// Ordenação crescente:
+nums.sort((a,b)=>a>b);
+console.log(nums);
+
+// Ordenação decrescente:
+nums.sort((a,b)=>a<b);
+console.log(nums);
+
+const nomes = ['Livia', 'Sandra', 'Deise', 'Marcos', 'Sandro'];
+nomes.sort();
+console.log(nomes);
