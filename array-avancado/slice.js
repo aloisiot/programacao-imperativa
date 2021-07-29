1a48a9fcd5aad61a04bd2a9ecbe8ea2eb3f30f4e
aloisiot <loistc.carvalho@gmail.com>
2021-07-29 12:30:53 -0300

    metodo delete para remover atributos de objetos e correção do aruivo que contem o metodo slice para arrays

--- array-avancado/slice.js
+++ array-avancado/slice.js
@@ -4,7 +4,7 @@
 const nums = [1,34,4353,23,55,5,63];
 
 const subNums = nums.slice(3,5);
-console.log(numsResum);
+console.log(subNums);
 
-// Obs: ao contrário do que se pode cpensar
+// Obs: ao contrário do que se pode pensar
 //      a interação retorna como ultimo elemento o elemento de indice final-1.s
---
+++ objetos/operador-delete.js
@@ -0,0 +1,6 @@
+// O operador delet remove uma propriedade de um objeto
+ let pessoa ={nome: 'Adão', sobrenome: 'Santos'}
+
+delete pessoa.sobrenome;
+
+console.log(pessoa);
